1a9e699305cc86a526291c623a3adf81dd720503
Baldinof <baldinof@gmail.com>
2020-08-30 19:07:50 +0200

    Run php-cs-fixer on services configuration file

--- config/services.php
+++ config/services.php
@@ -2,32 +2,27 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-
-use Spiral\RoadRunner\PSR7Client;
-use Spiral\Goridge\SocketRelay;
-use Spiral\Goridge\RelayInterface;
-use Baldinof\RoadRunnerBundle\Worker\Configuration;
-use Spiral\RoadRunner\Worker as RoadRunnerWorker;
-use Baldinof\RoadRunnerBundle\Worker\Worker;
-use Baldinof\RoadRunnerBundle\Worker\WorkerInterface;
-use Baldinof\RoadRunnerBundle\Worker\Dependencies;
-use Baldinof\RoadRunnerBundle\Http\MiddlewareStack;
+use Baldinof\RoadRunnerBundle\Command\WorkerCommand;
+use Baldinof\RoadRunnerBundle\DependencyInjection\BaldinofRoadRunnerExtension;
+use Baldinof\RoadRunnerBundle\EventListener\StreamedResponseListener;
+use Baldinof\RoadRunnerBundle\Http\IteratorRequestHandlerInterface;
 use Baldinof\RoadRunnerBundle\Http\KernelHandler;
 use Baldinof\RoadRunnerBundle\Http\Middleware\NativeSessionMiddleware;
+use Baldinof\RoadRunnerBundle\Http\MiddlewareStack;
+use Baldinof\RoadRunnerBundle\Metric\MetricFactory;
+use Baldinof\RoadRunnerBundle\Worker\Configuration;
+use Baldinof\RoadRunnerBundle\Worker\Dependencies;
+use Baldinof\RoadRunnerBundle\Worker\Worker;
+use Baldinof\RoadRunnerBundle\Worker\WorkerInterface;
+use Spiral\Goridge\RelayInterface;
+use Spiral\Goridge\SocketRelay;
+use Spiral\RoadRunner\MetricsInterface;
+use Spiral\RoadRunner\PSR7Client;
+use Spiral\RoadRunner\Worker as RoadRunnerWorker;
 use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
 use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
-use Psr\Http\Message\ServerRequestFactoryInterface;
-use Psr\Http\Message\StreamFactoryInterface;
-use Psr\Http\Message\UploadedFileFactoryInterface;
-use Psr\Http\Message\ResponseFactoryInterface;
 use Symfony\Bridge\PsrHttpMessage\HttpFoundationFactoryInterface;
 use Symfony\Bridge\PsrHttpMessage\HttpMessageFactoryInterface;
-use Baldinof\RoadRunnerBundle\Metric\MetricFactory;
-use Spiral\RoadRunner\MetricsInterface;
-use Baldinof\RoadRunnerBundle\EventListener\StreamedResponseListener;
-use Baldinof\RoadRunnerBundle\DependencyInjection\BaldinofRoadRunnerExtension;
-use Baldinof\RoadRunnerBundle\Command\WorkerCommand;
-use Baldinof\RoadRunnerBundle\Http\IteratorRequestHandlerInterface;
 
 return static function (ContainerConfigurator $container) {
     $services = $container->services();
@@ -41,7 +36,7 @@ return static function (ContainerConfigurator $container) {
         ->args([
             '%kernel.project_dir%/var/roadrunner.sock',
             null,
-            SocketRelay::SOCK_UNIX
+            SocketRelay::SOCK_UNIX,
         ]);
 
     $services->set(Configuration::class);
@@ -53,18 +48,16 @@ return static function (ContainerConfigurator $container) {
 
     $services->set(Dependencies::class)
         ->autowire()
-        ->public()
+        ->public() // Manually retrieved on the DIC in the Worker if the kernel has been rebooted
         ->arg('$requestHandler', ref(MiddlewareStack::class));
 
     $services->set(WorkerCommand::class)
         ->autowire()
         ->autoconfigure();
 
-
     $services->set(KernelHandler::class)
         ->autowire()
         ->arg('$kernel', ref('kernel'));
-
 
     $services->set(MiddlewareStack::class)
         ->autowire()
