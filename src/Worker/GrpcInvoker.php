44e7ecf75df2eae5a353b6477f575d805755005b
Mikael Titinovskii <mimits89@gmail.com>
2025-05-07 18:43:25 +0300

    Bring back old code

--- src/Worker/GrpcInvoker.php
+++ src/Worker/GrpcInvoker.php
@@ -78,29 +78,33 @@ final class GrpcInvoker implements InvokerInterface, GrpcTerminableInterface
             $this->gen = null;
         }
 
-        try {
-            if ($this->kernel->getContainer()->has('services_resetter')) {
-                /** @var ResetInterface $resetter */
-                $resetter = $this->kernel->getContainer()->get('services_resetter');
-                $resetter->reset();
-            }
-        } catch (\Throwable $e) {
-            $this->logger->error(
-                \sprintf(
-                    'An error occurred when resetting services: %s',
-                    $e->getMessage()
-                ),
-                ['exception' => $e]
-            );
-        }
-
         if ($this->kernel instanceof RebootableInterface && $this->dependencies->getKernelRebootStrategy()->shouldReboot()) {
+            try {
+                if ($this->kernel->getContainer()->has('services_resetter')) {
+                    /** @var ResetInterface $resetter */
+                    $resetter = $this->kernel->getContainer()->get('services_resetter');
+                    $resetter->reset();
+                }
+            } catch (\Throwable $e) {
+                $this->logger->error(
+                    \sprintf(
+                        'An error occurred when resetting services: %s',
+                        $e->getMessage()
+                    ),
+                    ['exception' => $e]
+                );
+            }
+
             $this->kernel->reboot(null);
             /** @var GrpcDependencies */
             $deps = $this->kernel->getContainer()->get(GrpcDependencies::class);
 
             $this->dependencies = $deps;
             $this->dependencies->getEventDispatcher()->dispatch(new WorkerKernelRebootedEvent());
+        } elseif ($this->kernel->getContainer()->has('services_resetter')) {
+            /** @var ResetInterface $resetter */
+            $resetter = $this->kernel->getContainer()->get('services_resetter');
+            $resetter->reset();
         }
 
         $this->dependencies->getKernelRebootStrategy()->clear();
