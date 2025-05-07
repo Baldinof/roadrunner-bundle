1a4ceb67ee7daa1c060a15015aa79752004f36db
Mikael Titinovskii <mimits89@gmail.com>
2025-05-07 16:08:57 +0300

    Small changes to log and try/catch

--- src/Worker/GrpcInvoker.php
+++ src/Worker/GrpcInvoker.php
@@ -79,21 +79,22 @@ final class GrpcInvoker implements InvokerInterface, GrpcTerminableInterface
         }
 
         if ($this->kernel instanceof RebootableInterface && $this->dependencies->getKernelRebootStrategy()->shouldReboot()) {
-            if ($this->kernel->getContainer()->has('services_resetter')) {
-                try {
+            try {
+                if ($this->kernel->getContainer()->has('services_resetter')) {
                     /** @var ResetInterface $resetter */
                     $resetter = $this->kernel->getContainer()->get('services_resetter');
                     $resetter->reset();
-                } catch (\Throwable $e) {
-                    $this->logger->error(
-                        \sprintf(
-                            'An error occurred when resetting services: %s',
-                            $e->getMessage()
-                        ),
-                        ['throwable' => $e]
-                    );
-                    throw $e;
                 }
+            } catch (\Throwable $e) {
+                $this->logger->error(
+                    \sprintf(
+                        'An error occurred when resetting services: %s',
+                        $e->getMessage()
+                    ),
+                    ['exception' => $e]
+                );
+
+                throw $e;
             }
 
             $this->kernel->reboot(null);
